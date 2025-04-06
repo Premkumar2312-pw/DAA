69898c754dcb735f5f198ecac0d6299f922951ac
Prem Kumar M <premkumarm23122005@gmail.com>
2025-04-06 18:38:03 +0530

    Update DpCoinChange.java

--- Java/DpCoinChange.java
+++ Java/DpCoinChange.java
@@ -1,99 +1,80 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
-class CoinChangeSolver{
-     private int[] coinValues;
-     private int totalAmount;
-     private int[][] minCoins;
-     private int[][] coinUsed;
-     
-     public CoinChangeSolver(int[] coinValues,int totalAmount){
-            this.coinValues = coinValues;
-            this.totalAmount = totalAmount;
-            int numCoins = coinValues.length;
-            minCoins = new int[numCoins + 1][totalAmount + 1];
-            coinUsed = new int[numCoins + 1][totalAmount + 1];
-     }
-     
-     public void solve(){
-            int numCoins = coinValues.length;
-            for(int amt=1;amt<= totalAmount;amt++){
-                 minCoins[0][amt] = totalAmount + 1;
+public class coinchangeDP {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of denominations: ");
+        int n = scanner.nextInt();
+        int[] denominations = new int[n];
+        System.out.println("Enter denominations:");
+        for (int i = 0; i < n; i++) {
+            denominations[i] = scanner.nextInt();
+        }
+        System.out.print("Enter target amount: ");
+        int amount = scanner.nextInt();
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int j = 1; j <= amount; j++) {
+            dp[0][j] = amount + 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j < denominations[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] =  Math.min(dp[i-1][j], 1+ dp[i][j-denominations[i-1]]);
+                }
             }
-           
-            for(int i=1;i<=numCoins;i++){
-                  for(int amt=0;amt<=totalAmount;amt++){
-                        if(amt == 0){
-                           minCoins[i][amt] = 0;
-                        }else if(coinValues[i - 1]<=amt){
-                              if(minCoins[i-1][amt] <= 1+minCoins[i][amt - coinValues[i-1]]){
-                                     minCoins[i][amt] = minCoins[i-1][amt];
-                                     coinUsed[i][amt] = coinUsed[i-1][amt];
-                              }else{
-                                     minCoins[i][amt] = 1 + minCoins[i][amt - coinValues[i-1]];
-                                     coinUsed[i][amt] = coinValues[i-1];
-                              }
-                        }else{
-                             minCoins[i][amt] = minCoins[i-1][amt];
-                             coinUsed[i][amt] = coinUsed[i-1][amt];
-                        }
-                  }
+        }
+        printTable(dp, denominations, amount);
+        if (dp[n][amount] == amount + 1) {
+            System.out.println("\nNo solution possible with given denominations.");
+            scanner.close();
+            return;
+        }
+        System.out.println("Minimum number of coins needed: " + dp[n][amount]);
+        List<Integer> usedCoins = new ArrayList<>();
+        int i = n, j = amount;
+        while (j > 0 && i > 0) {
+            if (dp[i][j] != dp[i-1][j]) {
+                usedCoins.add(denominations[i-1]);
+                j -= denominations[i-1];
+            } else {
+                i--;
             }
-     }
-     
-     public void printResult(){
-         int numCoins = coinValues.length;
-         int result = minCoins[numCoins][totalAmount];
-         
-         System.out.println("\nDP Table:");
-         for(int i=0;i<=numCoins;i++){
-              for(int amt=0;amt<=totalAmount;amt++){
-                    System.out.print(minCoins[i][amt] + "\t");
-              }
-              System.out.println();
-         }
-         
-         if(result > totalAmount){
-              System.out.println("No solution possible.");
-         }else{
-              System.out.println("Min coins required: "+result);
-              System.out.print("Coins used: ");
-             
-              int amt = totalAmount;
-             
-              while(amt > 0){
-                   int coin = coinUsed[numCoins][amt];
-                   if(coin == 0) break;
-                   System.out.print(coin + " ");
-                   amt -= coin;
-              }
-              System.out.println();              
-         }
-     }
-}
+        }
+        System.out.print("Denominations used: {");
+        for (int k = 0; k < usedCoins.size(); k++) {
+            System.out.print(usedCoins.get(k));
+            if (k < usedCoins.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("}");
 
-
-class Coinchange{
-     public static void main(String[] args){
-          Scanner sc = new Scanner(System.in);
-         
-          System.out.println("Enter the number of Denominations:");
-          int numCoins = sc.nextInt();
-          int[] coinValues = new int[numCoins];
-         
-          System.out.println("Enter the coin denominations: ");
-          for(int i=0;i<numCoins;i++){
-             coinValues[i] = sc.nextInt();
-          }    
-         
-          System.out.print("Enter the Amount: ");
-          int totalAmount = sc.nextInt();
-         
-          CoinChangeSolver solver = new CoinChangeSolver(coinValues,totalAmount);
-         
-          solver.solve();
-          solver.printResult();
-         
-          sc.close();
-     }
+        scanner.close();
+    }
+    private static void printTable(int[][] dp, int[] denominations, int amount) {
+        System.out.print("  T  ");
+        for (int j = 0; j <= amount; j++) {
+            System.out.printf("%3d", j);
+        }
+        System.out.println();
+        for (int i = 0; i <= denominations.length; i++) {
+            if (i == 0) {
+                System.out.print("  0  ");
+            } else {
+                System.out.printf("%3d  ", denominations[i - 1]);
+            }
+            for (int j = 0; j <= amount; j++) {
+                if (dp[i][j] == amount + 1) {
+                    System.out.printf("%3d", amount + 1);
+                } else {
+                    System.out.printf("%3d", dp[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
 }
