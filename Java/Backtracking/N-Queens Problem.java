2e05839978e3891ca89a6679b318ae07c7bda983
Prem Kumar M <premkumarm23122005@gmail.com>
2025-04-13 17:08:19 +0530

    Update N-Queens Problem.java

--- Java/Backtracking/N-Queens Problem.java
+++ Java/Backtracking/N-Queens Problem.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class NQueens {
     static int nqStep = 1;
-    static int N = 4;
+    static int N;
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N (number of queens): ");
+        N = sc.nextInt();
         int[][] board = new int[N][N];
         System.out.println("\nSolving N-Queens for N = " + N);
-        solveNQueens(board, 0);
+        boolean hasSolution = solveNQueens(board, 0);
+        if (!hasSolution) {
+            System.out.println("No solution exists for N = " + N);
+        }
+        sc.close();
     }
 
     static boolean solveNQueens(int[][] board, int row) {
@@ -35,13 +44,13 @@ public class NQueens {
 
     static void printBoard(int[][] board, String msg) {
         System.out.println(msg);
-        System.out.println("+---+---+---+---+");
+        System.out.println("+---".repeat(N) + "+");
         for (int[] row : board) {
             for (int cell : row) {
                 System.out.print("| " + (cell == 1 ? "Q" : " ") + " ");
             }
             System.out.println("|");
-            System.out.println("+---+---+---+---+");
+            System.out.println("+---".repeat(N) + "+");
         }
         System.out.println();
     }
